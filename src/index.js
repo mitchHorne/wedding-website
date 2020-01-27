2ee1df98777f3d0804ae26885662d914dce74634
mitch <mitchhorne92@gmail.com>
2020-01-27 20:37:32 +0200

    Add all the things

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Main from "./Main";
+import Dashboard from "./Dashboard";
+import "./index.css";
+import { theme } from "./theme";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Router = () => (
+  <BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <Switch>
+        <Route exact path="/">
+          <Main />
+        </Route>
+        <Route path="/adminDash">
+          <Dashboard />
+        </Route>
+      </Switch>
+    </ThemeProvider>
+  </BrowserRouter>
+);
+
+ReactDOM.render(<Router />, document.getElementById("root"));
---
+++ src/theme.js
@@ -0,0 +1,8 @@
+export const theme = {
+  colors: {
+    primary: "#ddd",
+    secondary: "#e5c100",
+    backgroundPrimary: "#fff",
+    backgroundSecondary: "#36454f"
+  }
+};
